4ba8757b5bb00d408c53e2cb11a0c613a9ac0f71
Gabrielcoutz <gabriel.coutinho.cassiano@gmail.com>
2026-01-21 19:18:24 -0300

    feat: update navigation types to include detailed parameters for home and filter screens

--- src/types/navigation.d.ts
+++ src/types/navigation.d.ts
@@ -1,13 +1,30 @@
+import { IOrderingFilters } from '@/components/OrderingFilters';
+import { IStackRoutes } from '@/routes/StackRoutes';
+import { IBudgetStatus } from '@/types/budget';
+
 /* eslint-disable @typescript-eslint/no-empty-object-type */
 export type RootStackParamList = {
-  home: undefined;
+  home:
+    | undefined
+    | {
+        status: IBudgetStatus[];
+        search: string;
+        orderBy: IOrderingFilters;
+      };
   budget: undefined | { id: string };
+  details: { id: string };
   service: undefined | { budgetId: string; serviceId: string };
-  filter: undefined;
+  filter:
+    | undefined
+    | {
+        status: IBudgetStatus[];
+        search: string;
+        orderBy: IOrderingFilters;
+      };
 };
 
 declare global {
   namespace ReactNavigation {
-    interface RootParamList extends RootStackParamList {}
+    interface RootParamList extends IStackRoutes {}
   }
 }
